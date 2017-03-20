fa2eed3ed7d0a69afe2834d00c0a949bd7bc2ada
Marcelo Pereira <mcpereira@rerum.com.br>
2017-03-19 21:17:03 -0300

    Exercio 6: Cálculo da área de um circulo

--- exercicio9/src/exercicio9/Exercicio9.java
+++ exercicio9/src/exercicio9/Exercicio9.java
@@ -4,8 +4,6 @@ import java.util.Scanner;
 
 /**
  *Escreva um algoritmo que leia três números decimais: A, B e C. Em seguida, calcule e mostre:
-
-b) a área do círculo de raio C. (π = 3.14159)
 c) a área do trapézio que tem A e B por bases e C por altura.
 d) a área do quadrado que tem lado B.
 e) a área do retângulo que tem lados A e B.
@@ -16,7 +14,7 @@ public class Exercicio9 {
 
     public static void main(String[] args) {
         Scanner leitor = new Scanner (System.in);
-        double a, b, c, calculo;
+        double a, b, c, calculo, exp, pi;
         
         System.out.println("insira o valor de A:");
         a = leitor.nextDouble();
@@ -30,6 +28,12 @@ public class Exercicio9 {
         //a) a área do triângulo retângulo que tem A por base e C por altura.
         calculo=(a*c)/2;
         System.out.println("A área do triângulo retângulo de base A e altura C é: " + calculo);
+        
+        //b) a área do círculo de raio C. (π = 3.14159)
+        exp = 2.0;
+        pi = 3.14159;
+        calculo = pi * (Math.pow(c, exp));
+        System.out.printf("A área de um ciruculo de ráio C é igual a %.2f \n", calculo);
 
     }
     
