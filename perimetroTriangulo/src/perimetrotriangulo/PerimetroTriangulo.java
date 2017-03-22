adf87d5989c70c51c9730a4fb43656d2f8899f7a
Marcelo Pereira <mcpereira@rerum.com.br>
2017-03-22 14:34:25 -0300

    Perimetro Triangulo

---
+++ perimetroTriangulo/src/perimetrotriangulo/PerimetroTriangulo.java
@@ -0,0 +1,18 @@
+package perimetrotriangulo;
+
+import java.util.Scanner;
+
+/**
+ * Escreva um programa em Java que lê o tamanho dos dois catetos de um triângulo 
+ * retângulo e que mostra o perímetro do triângulo.
+ * @author Marcelo Pereira
+ */
+public class PerimetroTriangulo {
+
+    
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        
+    }
+    
+}
