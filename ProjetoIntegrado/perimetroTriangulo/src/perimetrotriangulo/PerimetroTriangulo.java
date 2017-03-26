3f6461b52cd654b1fd82bf532d0cca6164a15528
Marcelo Pereira <mcpereira@rerum.com.br>
2017-03-26 17:44:11 -0300

    Perimetro Triângulo

--- ProjetoIntegrado/perimetroTriangulo/src/perimetrotriangulo/PerimetroTriangulo.java
+++ ProjetoIntegrado/perimetroTriangulo/src/perimetrotriangulo/PerimetroTriangulo.java
@@ -11,7 +11,20 @@ public class PerimetroTriangulo {
 
     
     public static void main(String[] args) {
+        
         Scanner entrada = new Scanner(System.in);
+        double cat1, cat2, hipo, p;
+        
+        System.out.println("Digite o valor do primeiro cateto:");
+        cat1 = entrada.nextDouble();
+        
+        System.out.println("Digite o valor do segundo cateto:");
+        cat2 = entrada.nextDouble();
+        
+        hipo = Math.sqrt((cat1*cat1)+(cat2*cat2));
+        p= cat1+cat2+hipo;
+        
+        System.out.println("O perimetro do triângulo é: "+ p);        
         
     }
     
