61c5b864db66577fa37d8659934ddee114c87557
marcelo.cpereira4 <marcelo.cpereira4@CAS555572W0719.senacsp.edu.br>
2017-04-04 22:44:51 -0300

    letras

---
+++ ProjetoIntegrado/letras/src/letras/Letras.java
@@ -0,0 +1,30 @@
+package letras;
+
+import java.util.Scanner;
+
+/**
+ * Escreva um programa que lê uma letra e mostra se ela é uma vogal ou consoante.
+ * Atenção: use o comando switch
+ * @author marcelo.cpereira4
+ */
+public class Letras {
+
+    public static void main(String[] args) {
+        Scanner console = new Scanner(System.in);
+        String letra =  console.next();
+        
+        switch(letra.toLowerCase().matches("[a-z]")){
+            case "a":
+            case "e":
+            case "i":
+            case "o":
+            case "u":
+                System.out.println("A letra "+letra+" é uma Volgal");
+                break;
+            default:
+                if(letra.matches("^0"))
+
+        }
+    }
+    
+}
