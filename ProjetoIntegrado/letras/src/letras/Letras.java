9f90d29e739d6b00e4b41b9e68b166f6aea19b37
Marcelo Pereira <mcpereira@rerum.com.br>
2017-04-05 23:43:55 -0300

    Exercício letras com switch e validação do que foi digitado

--- ProjetoIntegrado/letras/src/letras/Letras.java
+++ ProjetoIntegrado/letras/src/letras/Letras.java
@@ -3,28 +3,33 @@ package letras;
 import java.util.Scanner;
 
 /**
- * Escreva um programa que lê uma letra e mostra se ela é uma vogal ou consoante.
- * Atenção: use o comando switch
+ * Escreva um programa que lê uma letra e mostra se ela é uma vogal ou
+ * consoante. Atenção: use o comando switch
+ *
  * @author marcelo.cpereira4
  */
 public class Letras {
 
     public static void main(String[] args) {
         Scanner console = new Scanner(System.in);
-        String letra =  console.next();
-        
-        switch(letra.toLowerCase()){
-            case "a":
-            case "e":
-            case "i":
-            case "o":
-            case "u":
-                System.out.println("A letra "+letra+" é uma Volgal");
-                break;
-            default:
-                if(letra.matches("^0"))
-
+        System.out.print("Digite uma letra: ");
+        String letra = console.next();
+        if (letra.matches("[a-zA-Z]")) {
+            switch (letra.toLowerCase()) {
+                case "a":
+                case "e":
+                case "i":
+                case "o":
+                case "u":
+                    System.out.println("A letra " +"'"+ letra +"'"+ " é uma Vogal!");
+                    break;
+                default:
+                    System.out.println("A letra " +"'"+ letra+"'" + " é uma Consoante!");
+                    break;
+            }
+        } else {
+            System.out.println("Digite uma letra");
         }
+
     }
-    
 }
