081c61f0fc3acf245938b6b488b8678a4cde6baf
marcelo.cpereira4 <marcelo.cpereira4@CAS576216W1029.senacsp.edu.br>
2017-09-12 21:54:12 -0300

    Exercicio de sala - Classes e Objetos

---
+++ SegundoSemestre/ALP2/CofreMoedas/src/cofremoedas/Cofre.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cofremoedas;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author marcelo.cpereira4
+ */
+public class Cofre {
+    private ArrayList<Moedas> cofreMoeda;
+    
+    public void AddMoedas(Moedas moeda){
+        cofreMoeda.add(moeda);
+        
+    }
+    public float somaValoresMoedas(){
+        int soma = 0;
+        
+        cofreMoeda.
+    }
+}
---
+++ SegundoSemestre/ALP2/CofreMoedas/src/cofremoedas/Moedas.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cofremoedas;
+
+/**
+ *
+ * @author marcelo.cpereira4
+ */
+public class Moedas {
+    
+       public float valor;
+       
+       public Moedas(float valor){
+    
+           this.valor=valor;
+}
+       public float getValorMoeda(){
+           return this.valor;
+       }
+
+}
